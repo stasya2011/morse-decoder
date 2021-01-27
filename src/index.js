aa7119541ca388588ee5430def626d1d17f3c97d
Stasya Geleysha <s.geleisha@gmail.com>
2021-01-27 12:32:07 +0300

    i did this taskgit add .)

--- src/index.js
+++ src/index.js
@@ -39,8 +39,65 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const arr = [];
+    let str = "";  
+    for (let i = 0; i < expr.length; i = i + 10) {
+        arr.push(expr.substr(i, 10));
+    }
+    console.log(arr);
+
+    
+    let arrAfterMap = arr.map(element => {
+        for (let i = 0; i < element.length; i++) {
+            if (element[i] == 1) {
+               return element.substr(i);
+            }else if(element[i] == "*"){
+                return element
+            }
+
+        }
+       
+    });
+    console.log(arrAfterMap);
+
+
+
+    let d = arrAfterMap.map(element => {
+        for (let i = 0; i < element.length; i = i + 2) {
+            if (!element.includes("*")) {
+                str = element.substr(i, 2);
+                if (str == "10") {
+                    element += ".";
+                } else if (str == "11") {
+                    element += "-";
+                }
+            } else {
+                element = " ";
+                continue;
+            }
+
+        }
+        return element.replace(/\d/g, "");
+
+    });
+
+    //console.log(d);    
+    let dd = d.map(el=>{
+        for(let key in MORSE_TABLE){
+            if(el == key){
+                el = MORSE_TABLE[key];
+               // console.log(el);
+            }
+        }
+      return el;
+        
+    });
+    // console.log(dd);
+    // console.log(dd.join("").replace(/,/g, ""));
+    return dd.join("").replace(/,/g, "");
+    
 }
 
 module.exports = {
     decode
-}
+};
